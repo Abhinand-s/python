79705383e14261354abc6ef39b5c5c8ba12ca9fb
abhinands <abhinands488@gmail.com>
2024-07-17 10:54:29 +0530

    Day1

---
+++ day1/app.py
@@ -0,0 +1,39 @@
+# print("ABHINAND S")
+# print("hi welcome all")
+# print("o----")
+# print(" ||||")
+# print("*" * 10)
+# price = 11
+# print(price)
+# if price<=10 :
+#     print(price)
+# else :
+#     print("not")
+#
+# is_published = True
+# print(is_published)
+
+#problem 1
+
+# name = "john smith"
+# age = 20
+# exist = 0
+#
+# name1 = input('enter the name ')
+# print("enter the age")
+# age1 = input()
+# if name == name1 and age == age1 :
+#     exist =1
+# else :
+#     exist = 0
+#
+# if exist ==1 :
+#     print("found"+ name)
+# else :
+#     print("not found")
+
+#problem 2
+
+name = input("what's your name? ")
+car = input("what is your favourite car? ")
+print(name + " Like " + car)
